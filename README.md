39eef6788cffa8d8dea203587f2f3160b9e9aa19
worldofescapes <worldofescapes@gmail.com>
2022-02-05 19:12:37 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# convert_mysql_encoding
-Convert MySQL Encoding
+# convert_mysql_encoding.sh
+## _Convert MySQL Encoding_
+## How to use
+Clone  **convert_mysql_encoding**:
+
+> git clone git@github.com:worldquesta/convert_mysql_encoding.git
+
+
+Use script:
+```sh
+cd convert_mysql_encoding
+chmod 755 convert_mysql_encoding.sh
+./convert_mysql_encoding.sh bd_name.sql utf8mb4_unicode_ci utf8mb4
+```
+
+ - First  PARAM: Name BD
+ - Second PARAM: COLLATE for set in BD
+ - Third  PARAM: CHARSET for set in BD
+
+**That's all!**
---
+++ convert_mysql_encoding.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+
+if ([ -z "$1" ] || [ -z "$2" ] ||  [ -z "$3" ]) && [ "$2" != "SHOW" ]; then
+  printf "Error.\n=-\n"
+  echo '1) Example for run converting:'
+  echo './convert.sh bd_name.sql utf8mb4_unicode_ci utf8mb4'
+  echo ' - First  PARAM: Name BD'
+  echo ' - Second PARAM: COLLATE for set in BD'
+  echo ' - Third  PARAM: CHARSET for set in BD'
+  printf "\n"
+  echo '2) Example for show COLLATE and CHARSET:'
+  echo './convert.sh bd_name.sql SHOW'
+  echo ' - First  PARAM: Name BD'
+  echo ' - Second PARAM: Show'
+  printf "=-\nDone.\n"
+  exit 1
+fi
+
+
+BD=$1
+COLLATE=$2
+CHARSET=$3
+CHARACTER=$CHARSET
+
+FIELDS='(COLLATE|CHARSET|CHARACTER SET|SET NAMES)'
+
+
+if [ "$2" == "SHOW" ]; then
+  cat $BD | grep -Po "${FIELDS}(\s|=)[A-Za-z0-9-_]*" | sort | uniq | while IFS= read -r name; do echo $name; done
+elif [ -f "$BD" ]; then
+  printf "BD '$BD' founded. Start converting proccess..\n=-\n"
+
+  cat $BD | grep -Po "${FIELDS}(\s|=)[A-Za-z0-9-_]*($| |;|,|\t)" | sort | uniq | while IFS= read -r name; do
+    s1=$(echo $name | grep -oP "${FIELDS}(\s|=)")
+    s2=${name#"$s1"}
+    if [[ $name =~ ( |;|,|\t)$ ]]; then
+      e1=""; e2="${s2: -1}"
+    else
+      e1="$"; e2=""
+    fi
+    if [[ $s1 =~ (CHARSET|CHARACTER|NAMES) ]]; then 
+      echo "YOU CAN USE: /${name}${e1}/ s//${s1}${CHARACTER}${e2}/g"
+      sed -i "/${name}${e1}/ s//${s1}${CHARACTER}${e2}/g" $BD
+    elif [[ $s1 =~ COLLATE ]]; then
+      echo "YOU CAN USE: /${name}${e1}/ s//${s1}${COLLATE}${e2}/g"
+      sed -i "/${name}${e1}/ s//${s1}${COLLATE}${e2}/g" $BD
+    fi
+  done
+  printf "=-\nDone converting proccess.\n"
+else
+  echo "BD $BD not found or some went wrong"
+fi
+
